0cc3bcc11c70698916b856466608f847ec190522
Stefan Dworschak <stef.dworschak@gmail.com>
2021-12-04 23:20:47 +0000

    Adding challenge 2 solution

--- day4/day4.py
+++ day4/day4.py
@@ -12,8 +12,10 @@ def clean_data(data):
         if (i+1) % 5 == 0:
            cards.append(card[:-1])
            card = ''
-    checks = [','.join(len(card.split(',')) * ['0']) for j, card in enumerate(cards)]
+    checks = [','.join(len(card.split(',')) * ['0'])
+                           for j, card in enumerate(cards)]
     return numbers, cards, checks
+
 
 def mark_card(number, cards, checks):
     for i, card in enumerate(cards):
@@ -24,22 +26,41 @@ def mark_card(number, cards, checks):
                 checks[i] = ','.join(_checks)
                 continue
 
+
 def check_win(checks):
-    # import ipdb; ipdb.set_trace()
     return any(True for c in checks if sum([int(i) for i in c]) == 5)
+
 
 def chunk(checks, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(checks), n):
         yield checks[i:i + n]
 
-def check_cards(checks):
+
+def check_multiple_cards(checks, first_wins):
+    winning_cards = []
     for i, check in enumerate(checks):
         _check = check.split(',')
         check_rows = list(chunk(_check, 5))
         check_cols = [_check[i::5] for i in range(int(len(_check)/5))]
         if check_win(check_rows) or check_win(check_cols):
-            return i
+            if first_wins:
+                return [i]
+            else:
+                winning_cards.append(i)
+    return winning_cards
+
+
+def check_winning(winning, cards, checks, first_wins):
+    for i, w in enumerate(winning):
+        if i > 0:
+            _winning = check_multiple_cards(checks, first_wins)
+            w = _winning[0]
+        winning_card = cards.pop(w)
+        winning_check = checks.pop(w)
+        if first_wins:
+            return winning_card, winning_check
+    return winning_card, winning_check
 
 
 def challenge(filepath, first_wins=True):
@@ -49,18 +70,18 @@ def challenge(filepath, first_wins=True):
     for number in numbers:
         used_numbers.append(number)
         mark_card(number, cards, checks)
-        winning = check_cards(checks)
-        if winning is not None:
-            # print(cards)
-            winning_card = cards.pop(winning)
-            winning_check = checks.pop(winning)
-            # print(winning_card)
-            if first_wins:
-                break
-            elif not cards:
-                break
+        winning = check_multiple_cards(checks, first_wins)
+        if not winning:
+            continue
+
+        winning_card, winning_check = check_winning(
+            winning, cards, checks, first_wins)
+
+        if first_wins:
+            break
+        elif not cards:
+            break
                 
-    
     unused_numbers = [int(n) for i, n in enumerate(winning_card.split(','))
                       if winning_check.split(',')[i] == '0']
     return sum(unused_numbers) * int(used_numbers[-1])
@@ -68,4 +89,4 @@ def challenge(filepath, first_wins=True):
 
 if __name__ == '__main__':
     print(f"Challenge 1 Result: {challenge('day4/day4.txt')}")
-    # print(f"Challenge 2 Result: {challenge('day4/day4.txt', False)}")
+    print(f"Challenge 2 Result: {challenge('day4/day4.txt', False)}")
